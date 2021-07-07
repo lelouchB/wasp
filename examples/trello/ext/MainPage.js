313aea019090ff3decd3317c724ca44acec84e24
Matija Sosic <matija.sosic@gmail.com>
2021-07-07 16:26:39 +0200

    Added FE for adding a list.

--- examples/trello/ext/MainPage.js
+++ examples/trello/ext/MainPage.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import { Plus } from 'react-feather'
+import React, { useState } from 'react'
+import { Plus, X } from 'react-feather'
 
 import UserPageLayout from './UserPageLayout'
 
@@ -13,20 +13,67 @@ const MainPage = ({ user }) => {
       <span>This is the beginning of our new Trello app - Waspello.</span>
 
       <div id='board'>
-
-        <div className='add-list list-wrapper'>
-          <button className='open-add-list'>
-              <div className='add-list-icon'>
-                <Plus size={16} strokeWidth={2} />
-              </div>
-              Add a list
-          </button>
-        </div>
-
+        <AddList />
       </div>
 
     </UserPageLayout>
   )
+}
+
+const AddList = () => {
+
+    const [isInEditMode, setIsInEditMode] = useState(false)
+
+    const AddListButton = () => {
+      return (
+        <button
+          className='open-add-list'
+          onClick={() => setIsInEditMode(true)}
+        >
+            <div className='add-list-icon'>
+              <Plus size={16} strokeWidth={2} />
+            </div>
+            Add a list
+        </button>
+      )
+    }
+
+    const AddListInput = () => {
+      const [titleFieldVal, setTitleFieldVal] = useState('')
+
+      const handleAddList = async (event) => {
+        event.preventDefault()
+        // Do API call.
+      }
+
+      return (
+        <form onSubmit={handleAddList}>
+          <input
+            className="list-name-input"
+            type="text"
+            placeholder="Enter list title..."
+            value={titleFieldVal}
+            onChange={e => setTitleFieldVal(e.target.value)}
+          />
+          <div className='list-add-controls'>
+            <input className='list-add-button' type='submit' value='Add list' />
+            <div
+              className='list-cancel-edit'
+              onClick={() => setIsInEditMode(false)}
+            >
+              <X  />
+            </div>
+          </div>
+        </form>
+      )
+    }
+
+    return (
+      <div className='add-list list-wrapper'>
+        { isInEditMode ? <AddListInput /> : <AddListButton /> }
+      </div>
+    )
+
 }
 
 export default MainPage
