06d4c3c23a8353dd6f2cf023c527e09200fdd77b
Matija Sosic <matija.sosic@gmail.com>
2021-07-07 12:42:18 +0200

    Added list entity and 'add list' button.

--- examples/trello/ext/MainPage.js
+++ examples/trello/ext/MainPage.js
@@ -1,14 +1,32 @@
 import React from 'react'
+import { Plus } from 'react-feather'
+
+import UserPageLayout from './UserPageLayout'
+
 import waspLogo from './waspLogo.png'
 import './Main.css'
 
-import UserPageLayout from './UserPageLayout'
 
 const MainPage = ({ user }) => {
   return (
     <UserPageLayout user={user}>
       <span>This is the beginning of our new Trello app - Waspello.</span>
+
+      <div id='board'>
+
+        <div className='add-list list-wrapper'>
+          <button className='open-add-list'>
+              <div className='add-list-icon'>
+                <Plus size={16} strokeWidth={2} />
+              </div>
+              Add a list
+          </button>
+        </div>
+
+      </div>
+
     </UserPageLayout>
   )
 }
+
 export default MainPage
